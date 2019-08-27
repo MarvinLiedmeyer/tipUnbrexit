9300ce9f4abbf25130ddd34f504e70dfa0593bec
Marvin Liedmeyer <marvin@liedmeyer.com>
2019-08-27 18:02:29 +0200

    :building_construction: updateStbansicht

--- src/components/Stb-Components/LastSpender.jsx
+++ src/components/Stb-Components/LastSpender.jsx
@@ -7,56 +7,58 @@ import { List, ListItem } from 'chayns-components';
 class LastSpender extends PureComponent {
     render() {
         return (
-            <List>
-                <ListItem
-                    title={(<h2>Leonhard Driesch</h2>)}
-                    image="http://placekitten.com/200/300"
-                    overflow="hidden"
-                    right={(
-                        <div
-                            style={{
-                                marginLeft: '10px',
-                                // opacity: '0.5',
-                                backgroundColor: 'white'
-                            }}
-                        >
-                            <h1> 2£ </h1>
-                        </div>
-                    )}
-                />
-                <ListItem
-                    title={(<h2>Leonhard Driesch</h2>)}
-                    image="http://placekitten.com/200/300"
-                    overflow="hidden"
-                    right={(
-                        <div
-                            style={{
-                                marginLeft: '10px',
-                                // opacity: '0.5',
-                                backgroundColor: 'white'
-                            }}
-                        >
-                            <h1> 10£ </h1>
-                        </div>
-                    )}
-                />
-                <ListItem
-                    title={(<h2>Leonhard Driesch</h2>)}
-                    image="http://placekitten.com/200/300"
-                    overflow="hidden"
-                    right={(
-                        <div
-                            style={{
-                                marginLeft: '10px',
-                                // opacity: '0.5',
-                                backgroundColor: 'white'
-                            }}
-                        >
-                            <h1> 2£ </h1>
-                        </div>
-                    )}
-                />
-            </List>
+            <div className="list">
+                <List>
+                    <ListItem
+                        title={(<h2>Leonhard Driesch</h2>)}
+                        image="http://placekitten.com/200/300"
+                        overflow="hidden"
+                        right={(
+                            <div
+                                style={{
+                                    marginLeft: '5%',
+                                    // opacity: '0.5',
+                                    backgroundColor: 'white'
+                                }}
+                            >
+                                <h1> 2£ </h1>
+                            </div>
+                        )}
+                    />
+                    <ListItem
+                        title={(<h2>Leonhard Driesch</h2>)}
+                        image="http://placekitten.com/200/300"
+                        overflow="hidden"
+                        right={(
+                            <div
+                                style={{
+                                    marginLeft: '5%',
+                                    // opacity: '0.5',
+                                    backgroundColor: 'white'
+                                }}
+                            >
+                                <h1> 10£ </h1>
+                            </div>
+                        )}
+                    />
+                    <ListItem
+                        title={(<h2>Leonhard Driesch</h2>)}
+                        image="http://placekitten.com/200/300"
+                        overflow="hidden"
+                        right={(
+                            <div
+                                style={{
+                                    marginLeft: '5%',
+                                    // opacity: '0.5',
+                                    backgroundColor: 'white'
+                                }}
+                            >
+                                <h1> 2£ </h1>
+                            </div>
+                        )}
+                    />
+                </List>
+            </div>
         );
     }
 }
